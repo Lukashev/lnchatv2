b6e542225385b89ba35b190290159bc5fa1f2ea8
Lukashev <lnv161297@gmail.com>
2021-03-09 19:38:13 +0200

    ready for production

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(express.static(__dirname + '/build'))
 
-app.use(logger('dev'))
+app.use(logger('dev', { skip: () => process.env.NODE_ENV === 'production' }))
 app.use('/api', apiRouter)
 app.use('*', (req, res) => {
   res.sendFile(`${__dirname}/build/index.html`)
